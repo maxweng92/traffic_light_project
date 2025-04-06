0fa0e598960e4c0771d386f8d267291ecc19b9b2
111116024 <s111116024@stu.ntue.edu.tw>
2025-04-06 16:25:24 +0800

    NEW - 可辨識箭頭

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ cv2.namedWindow("Original Video", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("Original Video", window_width, window_height)
 
 # identify_colors
-def identify_colors(mask, color_name):
+def identify_colors(mask,image, color_name):
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     if contours:
         for contour in contours:
@@ -18,14 +18,42 @@ def identify_colors(mask, color_name):
         largest_area = cv2.contourArea(largest_contour)  # 計算最大輪廓的面積
         x, y, w, h = cv2.boundingRect(largest_contour)  # 獲取邊界框
         aspect_ratio = w / float(h)  # 計算長寬比
-        print("Aspect_ratio:", aspect_ratio)
-        if 0.8 <= aspect_ratio <= 1.3:  # 接近正方形
-            if largest_area > 500:  # 過濾雜訊
-                return color_name, largest_area
-            # return color_name, largest_area
+        if 0.8 <= aspect_ratio <= 1.3:
+            if color_name == "Red" or color_name == "Yellow":
+                if largest_area > 500:
+                    return color_name, largest_area
             elif color_name == "Green":
-                if largest_area > 50:  # 過濾雜訊
-                    return ">", largest_area
+                if largest_area > 150:
+                    results = model5(image)
+                    ArrowSequence = ["<", "^", ">"] #表示三種class的方向
+                    for result in results:
+                        if hasattr(result, 'probs') and result.probs is not None:
+                            confidences = [round(prob.item(), 4) for prob in result.probs.data]
+                            predicted_class = result.probs.top1  # 最高概率的类别索引
+                            max_confidence = confidences[predicted_class]  # 最高置信度
+                            ans = ArrowSequence[predicted_class]  # 对应的类别名称
+
+                            # 打印所有置信度
+                            print("All confidences:")
+                            for idx, conf in enumerate(confidences):
+                                class_name = ArrowSequence[idx]  # 假设 ArrowSequence 是类别名称列表
+                                print(f"  {class_name}: {conf}")
+
+                            # print(f"Predicted Class: {ans}, Confidence: {max_confidence}")
+                            confidence = round(result.probs.data[predicted_class].item(), 2)  # 直接保留兩位小數
+                            ans = ArrowSequence[predicted_class]
+                            print(f"Predicted Class: {ans}, Confidence: {confidence}")
+                            # cv2.imshow("Green Pre", mask)
+                            # cv2.waitKey(0)
+                            # cv2.destroyWindow("Green Pre")
+                            if confidence > 0.7:
+                                return ans, confidence
+                        else:
+                            print("No classification results found.")
+                            return "X", 0.0
+
+                elif largest_area > 400:  # 過濾雜訊
+                    return color_name, largest_area
     return None, 0
 
 # split_box
@@ -68,9 +96,9 @@ def find_traffic_light(image,c,conf):
         mask_green = cv2.inRange(hsv, lower_green, upper_green)
 
         # 先嘗試識別不同燈號
-        red_result = identify_colors(mask_red, "Red")
-        yellow_result = identify_colors(mask_yellow, "Yellow")
-        green_result = identify_colors(mask_green, "Green")
+        red_result = identify_colors(mask_red,frame, "Red")
+        yellow_result = identify_colors(mask_yellow,frame, "Yellow")
+        green_result = identify_colors(mask_green,frame, "Green")
 
         # 判斷目前燈號
         light_BGR = (255, 255, 255)
@@ -88,9 +116,6 @@ def find_traffic_light(image,c,conf):
             light_colors.append(green_result[0])
             light_BGR = (0, 255, 0)
             largest_area = green_result[1]
-            cv2.putText(mask_green, f"{c}", (0, 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, light_BGR, 2)
-            cv2.imshow("Green Mask", mask_green)
-            cv2.waitKey(0)
 
         print(f"目前的紅綠燈狀態：{light_colors}")
 
@@ -117,7 +142,7 @@ def find_traffic_light(image,c,conf):
     
 # yoloVideo
 def video(image):
-    result = model(image)
+    result = model.predict(image, classes=[0, 1, 2, 3, 5, 7, 9])
     cv2.imshow("Original Video",result[0].plot())
 
 
@@ -162,6 +187,9 @@ def video(image):
 
 # Startup
 model = YOLO("YOLOv8l.pt")
+# model5 = YOLO("best_ARROW.pt").to("cuda")
+# model5 = YOLO("bestSingleArrowE40.pt")
+model5 = YOLO("bestSingleArrowTrain12.pt")
 target_classes = [0, 1, 2, 3, 5, 7, 9]  
 # print(model.names)
 
@@ -169,14 +197,14 @@ target_classes = [0, 1, 2, 3, 5, 7, 9]
 lower_red1, upper_red1 = np.array([0, 100, 100]), np.array([10, 255, 255])
 lower_red2, upper_red2 = np.array([160, 100, 100]), np.array([180, 255, 255])
 lower_yellow, upper_yellow = np.array([12, 100, 20]), np.array([35, 255, 255])
-lower_green, upper_green = np.array([50, 140, 90]), np.array([90, 255, 255])
+lower_green, upper_green = np.array([65, 155, 90]), np.array([90, 255, 255])
 # lower_green = np.array([40, 100, 100])
 # upper_green = np.array([90, 255, 255])
 
 # 開啟影片
-video_path = "video/2025-01-19_11-47-10-front - Trim.mp4"
+# video_path = "video/2025-01-19_11-47-10-front - Trim.mp4"
 # video_path = "video/color_changed.mp4"
-# video_path = "video/arrow_test.mp4"
+video_path = "video/arrow_test.mp4"
 cap = cv2.VideoCapture(video_path)
 
 frame_count = 0  # 計數器
